ea41996c064ea8d0c90bffd0525c43e71be87d8d
王敏 <569049370@qq.com>
2020-04-08 11:57:01 +0800

    install q1-1 components

--- Q1/src/pages/index/index.vue
+++ Q1/src/pages/index/index.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
     <div>I am Q1main project</div>
+    <Room></Room>
   </div>
 </template>
 
 <script>
+import Room from 'q1-1/src/components/room'
 
 export default {
   data () {
     return {
     }
   },
-
+  components: {
+    Room
+  },
   methods: {}
 }
 </script>
