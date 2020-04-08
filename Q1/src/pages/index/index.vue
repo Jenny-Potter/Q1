9dc6059bb87629fa58bcbf9a6c5c4fa86afe1d48
王敏 <569049370@qq.com>
2020-04-08 11:05:15 +0800

    init Q1project and build it self

---
+++ Q1/src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>I am Q1main project</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
